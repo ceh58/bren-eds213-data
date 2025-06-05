87c1271b8ed4af61c5f0edbe920da3d20dcfeb2e
ceh58 <hoyt.carmen@gmail.com>
2025-06-05 22:31:00 +0000

    Update assignment 9.

--- Week-10/assignment-9.sql
+++ Week-10/assignment-9.sql
@@ -1,7 +1,6 @@
-/courses/EDS213/big-fat.sqlite3
-
 -- move db to current directory
 cp /courses/EDS213/big-fat.sqlite3 .
+--- don't forget to add to .gitignore
 
 -- query from assignment
 SELECT Nest_ID
@@ -16,11 +15,10 @@ SELECT Nest_ID
 
 -- Part 1
 --- 1. Is there already an index on the Bird_nests table? No?
-.schema Bird_nests;
-
-SELECT name FROM sqlite_master WHERE type='index' AND tbl_name='Bird_nests';
+--- There is an automatic index for rows, but no index for values in any of the columns.
 
 --- If so, what is that index and will SQLite use it in the above query? Why or why not?
+--- The row index will not be used in the above query because it is searching for values.
 
 --- 2. Will adding an index on a column not mentioned in the WHERE clause be used by the database? Why or why not?
 --- No, because it will only search the index for the columns called in WHERE.
@@ -102,23 +100,3 @@ sqlite3 big-fat.sqlite3 "DROP INDEX index_site_species_year_obs;"
 sqlite3 big-fat.sqlite3 "CREATE INDEX index_site_species_year_obs_ageMethod ON Bird_nests(Site, Species, Year, Observer, ageMethod);"
 bash query_timer_9.sh "Site,Species,Year,Observer,ageMethod" 1 "$query" big-fat.sqlite3 assignment-9.csv    
 sqlite3 big-fat.sqlite3 "DROP INDEX index_site_species_year_obs_ageMethod;"
-
-
-
-
-
-
-
-
-SELECT Nest_ID FROM Bird_nests 
-WHERE Site = 'nome' 
-AND Species = 'ruff' 
-AND Year = 1983 
-AND Observer = 'cbishop' AND ageMethod = 'float';
-
-SELECT COUNT(*) FROM (
-    SELECT Nest_ID FROM Bird_nests 
-    WHERE Site = 'nome' 
-    AND Species = 'ruff' 
-    AND Year = 1983 
-    AND Observer = 'cbishop' AND ageMethod = 'float') AS a;
